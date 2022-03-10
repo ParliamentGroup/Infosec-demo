085a85065a379d79329c250e301fed13b20631bd
ParliamentGroup <98899446+ParliamentGroup@users.noreply.github.com>
2022-03-10 14:02:58 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,14 @@
 const express = require('express');
+
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+
+// ...
+
 const app = express();
 
 
